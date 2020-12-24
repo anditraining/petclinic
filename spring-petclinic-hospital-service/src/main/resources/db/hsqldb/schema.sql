6b9c39011463eab7c1cae6b37d964cf43dd08463
Andi Noviandi <andin@amdocs.com>
2020-12-24 11:44:59 +0700

    update

--- spring-petclinic-hospital-service/src/main/resources/db/hsqldb/schema.sql
+++ spring-petclinic-hospital-service/src/main/resources/db/hsqldb/schema.sql
@@ -2,7 +2,7 @@ DROP TABLE appointment IF EXISTS;
 
 CREATE TABLE appointment (
   id          INTEGER IDENTITY PRIMARY KEY,
-  appintment_date  DATE,
+  appointment_date  DATE,
   description VARCHAR(8192),
   pet_id      INTEGER NOT NULL
 );
